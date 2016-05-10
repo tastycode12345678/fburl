34e4aba379923bd696614b57ff4c52325680d4f4
Girish Kumar <girish.kumar@extentia.com>
2016-05-10 10:40:10 +0530

    Fisrt Commit

---
+++ heroku/index.js
@@ -0,0 +1,48 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
+});
+
+/*app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+  console.log(req.body);
+  // Process the Facebook updates here
+  res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  res.sendStatus(200);
+});*/
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+	res.sendStatus(200);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.listen();
